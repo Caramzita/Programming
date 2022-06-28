75ee939a216667990c6993b4b71acaefff849840
Caramzita <99469986+Caramzita@users.noreply.github.com>
2022-06-28 17:06:52 +0600

    Add files via upload

---
+++ FlightsApp/FlightsApp/Model/FlightType.cs
@@ -0,0 +1,11 @@
+﻿namespace FlightsApp.Model
+{
+    /// <summary>
+    /// Хранит перечисления типов перелета.
+    /// </summary>
+    public enum FlightType
+    {
+        Interior,
+        International
+    }
+}
