ff5e1af58641df04e57cc579910ff26ab1ecd46d
Никита Желнов <sosu_5_1@mail.ru>
2023-03-14 03:17:34 +0700

    complete second lab

---
+++ src/Contacts/View/Model/Services/ContactSerializer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace View.Model.Services
+{
+    internal static class ContactSerializer
+    {
+        public static string FilePath
+        {
+            get
+            {
+                return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Contacts\";
+            }
+        }
+
+        private static readonly string _contactData = @"contacts.json";
+
+        public static void SaveData(Contact contact)
+        {
+            // Создаём экземпляр сериализатора
+            JsonSerializer serializer = new JsonSerializer
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Include,
+                TypeNameHandling = TypeNameHandling.All
+            };
+
+            // Проверка наличия пути сохранения
+            String folder = FilePath;
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            // Открываем поток для записи в файл с указанием п
+            using (StreamWriter sw = new StreamWriter(FilePath + _contactData))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+
+            {
+                // Вызываем десериализацию и явно преобразуем результат в тип данных
+                serializer.Serialize(writer, contact);
+            }
+        }
+
+        public static Contact LoadDataFromFile()
+        {
+            // Создаём переменную, в которую поместим результат десериализации
+            Contact contact = null;
+
+            // Создаём экземпляр сериализатора
+            JsonSerializer serializer = new JsonSerializer
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Include,
+                TypeNameHandling = TypeNameHandling.All
+            };
+
+            String folder = FilePath;
+
+            // Проверка наличия пути загрузки.
+            if (!Directory.Exists(folder))
+            {
+                return new Contact();
+            }
+
+            // Открываем поток для чтения из файла с указанием пути
+            using (StreamReader sr = new StreamReader(FilePath + _contactData))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                // Вызываем десериализацию и явно преобразуем результат в целевой тип данных
+                contact = serializer.Deserialize<Contact>(reader);
+            }
+
+            return contact;
+        }
+    }
+}
---
+++ src/Contacts/View/ViewModel/MainVM.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using View.Model;
+using View.Model.Services;
+
+namespace View.ViewModel
+{
+    internal class MainVM : INotifyPropertyChanged
+    {
+        private Contact _currentContact = new Contact();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private SaveCommand saveCommand;
+
+        public SaveCommand SaveCommand
+        {
+            get
+            {
+                return saveCommand ??
+                  (saveCommand = new SaveCommand(obj =>
+                  {
+                      ContactSerializer.SaveData(CurrentContact);
+                  }));
+            }
+        }
+
+        private LoadCommand loadCommand;
+
+        public LoadCommand LoadCommand
+        {
+            get
+            {
+                return loadCommand ??
+                  (loadCommand = new LoadCommand(obj =>
+                  {
+                      CurrentContact = ContactSerializer.LoadDataFromFile();
+                  }));
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _currentContact.Name;
+            }
+            set
+            {
+                _currentContact.Name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return _currentContact.PhoneNumber;
+            }
+            set
+            {
+                _currentContact.PhoneNumber = value;
+                OnPropertyChanged("PhoneNumber");
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _currentContact.Email;
+            }
+            set
+            {
+                _currentContact.Email = value;
+                OnPropertyChanged("Email");
+            }
+        } 
+        
+        public Contact CurrentContact
+        {
+            get
+            {
+                return _currentContact;
+            }
+            set
+            {
+                _currentContact = value;
+                OnPropertyChanged("CurrentContact");
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }   
+}
